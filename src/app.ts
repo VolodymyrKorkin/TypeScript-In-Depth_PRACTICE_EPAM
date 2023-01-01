ca89e28d2691081c3e264db8d28c212a2b16d5c4
Vladimir Korkin <korkin.dev@gmail.com>
2023-01-01 12:16:41 +0100

    Types Basics. Add solution task 02.01.02

--- src/app.ts
+++ src/app.ts
@@ -24,19 +24,51 @@ function showHello(divName: string, name: string) {
 // -------------------------------------------------------------------------------------------------
 
 
-type Book = {
+// type Book = {
+//     id: number;
+//     title: string;
+//     author: string;
+//     available: boolean;
+// };
+
+// function getAllBooks(): Book[] {
+//     const books: Book[] = [
+//         { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true},
+//         { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
+//         { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true },
+//         { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true }
+//     ];
+//     return books;
+// }
+
+type Books = {
     id: number;
     title: string;
     author: string;
     available: boolean;
-};
+}[];
 
-function getAllBooks(): Book[] {
-    const books: Book[] = [
+function getAllBooks(): Books {
+    const books: Books = [
         { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true},
         { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
         { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true },
         { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true }
     ];
     return books;
+}
+// console.log(getAllBooks());
+
+
+// 02.01.02 ----------------------------------------------------------------------------------------
+// 2.	Реалізуйте функцію logFirstAvailable(), яка приймає масив книг як параметр і виводить у консоль:
+//    a.	кількість книг у масиві
+//    b.	назву першої доступної книги
+// -------------------------------------------------------------------------------------------------
+
+function logFirstAvailable(books: Books): void {
+    console.log(`Number of books: ${books.length}`);
+
+    const title = books.find(book => book.available === true).title;
+    console.log(`First available book: ${title}`);
 }
