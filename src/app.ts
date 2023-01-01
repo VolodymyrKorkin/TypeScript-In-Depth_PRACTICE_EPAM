a89efbc53640e6030ab52206224d29489fe7e094
Vladimir Korkin <korkin.dev@gmail.com>
2023-01-01 15:00:11 +0100

    Types Basics. Add solution task 02.01.05

--- src/app.ts
+++ src/app.ts
@@ -100,12 +100,12 @@ type Books = {
     category: Category;
 }[];
 
-// function getAllBooks(): Books {
-//     const books: Books = [
-//         { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true},
-//         { id: 2, title: 'JavaScript Testing', category: Category.JavaScript, author: 'Liang Yuxian Eugene', available: false },
-//         { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
-//         { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
-//     ];
-//     return books;
-// }
+function getAllBooks(): Books {
+    const books: Books = [
+        { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true},
+        { id: 2, title: 'JavaScript Testing', category: Category.JavaScript, author: 'Liang Yuxian Eugene', available: false },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
+    ];
+    return books;
+}
