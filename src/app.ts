5dd1b8120dfa9962e94dd2167d6804f183e18ea4
Vladimir Korkin <korkin.dev@gmail.com>
2023-01-02 16:43:08 +0100

    Const Assertions. Add solution task 02.02.01

--- src/app.ts
+++ src/app.ts
@@ -193,3 +193,34 @@ function calcTotalPages(): void {
 
     console.log(result);
 }
+
+
+// ==================================================================================================
+// Task 02.02. Const Assertions
+// ==================================================================================================
+
+// 02.02.01 ----------------------------------------------------------------------------------------
+// 1.	Додайте const assertions (<const>) для масиву книг та масиву,
+// який надає інформацію про сторінки книг у бібліотеках міста.
+// -------------------------------------------------------------------------------------------------
+type Book = {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+    category: Category;
+};
+
+function getAllBooks2(): readonly Book[] {
+    const books2 = <const> [
+        { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true},
+        { id: 2, title: 'JavaScript Testing', category: Category.JavaScript, author: 'Liang Yuxian Eugene', available: false },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
+    ];
+    return books2;
+}
+
+
+
+
