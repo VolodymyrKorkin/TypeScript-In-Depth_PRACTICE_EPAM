d317cdf2f2fefb03f8bdcdcfd1eb2ef506b640a8
Vladimir Korkin <korkin.dev@gmail.com>
2023-01-04 20:16:12 +0100

    Defining an Interface. Add solution task 04.01.02

--- src/app.ts
+++ src/app.ts
@@ -518,3 +518,21 @@ interface Book {
     available: boolean;
     category: Category;
 }
+
+// 04.01.02 ---------------------------------------------------------------------------------
+// 2.	Внесіть зміни в функцію getAllBooks(), вкажіть тип змінної books і тип значення,
+// що повертається, використовуючи оголошений вище інтерфейс Book. Додайте модифікатор readonly.
+// Видаліть тимчасово id у книжки та побачите, що з'явиться помилка.
+// ------------------------------------------------------------------------------------------
+
+function getAllBooks3(): readonly Book[] {
+    const books2 = <const> [
+        { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true},
+        { id: 2, title: 'JavaScript Testing', category: Category.JavaScript, author: 'Liang Yuxian Eugene', available: false },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
+    ];
+    return books2;
+}
+
+
